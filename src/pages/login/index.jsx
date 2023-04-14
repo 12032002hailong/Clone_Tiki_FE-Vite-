41fc7a7df58dc161d7d66bdbe6054feaf10e3fa2
Hai Longe <hailongkc1203@example.com>
2023-04-14 23:07:47 +0700

    Register + Login + Access Token + Refresh Token

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,96 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Button,
+  Divider,
+  Form,
+  Input,
+  Typography,
+  message,
+  notification
+} from "antd";
+import { useNavigate, Link } from "react-router-dom";
+import { callLogin } from "../../services/api";
 
 const LoginPage = () => {
-  return (
-    <div>
-      login page
-    </div>
-  )
-}
+  const navigate = useNavigate();
+  const [isSubmit, setIsSubmit] = useState(false);
 
-export default LoginPage
+  const onFinish = async (values) => {
+    const { username, password } = values;
+    setIsSubmit(true);
+    const res = await callLogin(username, password);
+    setIsSubmit(false);
+    if (res?.data) {
+      localStorage.setItem("access_token:", res.data.access_token);
+      console.log(">>> check res: ", res);
+      message.success("Đăng nhập tài khoản thành công!");
+      navigate("/");
+    } else {
+      notification.error({
+        message: "Có lỗi xảy ra",
+        descritption:
+          res.message && Array.isArray(res.message)
+            ? res.message[0]
+            : res.message,
+        duration: 5
+      });
+    }
+  };
+
+  return (
+    <div className="register-page">
+      <Form
+        className="loginForm"
+        name="basic"
+        labelCol={{ span: 6 }}
+        style={{ maxWidth: 600, margin: "0 auto" }}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Typography.Title>Đăng Nhập</Typography.Title>
+        <Divider></Divider>
+
+        <Form.Item
+          label="Email:"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "email không được để trống"
+            }
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password:"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password"
+            }
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item style={{ textAlign: "center" }}>
+          <Button type="primary" htmlType="submit" loading={isSubmit}>
+            Đăng nhập
+          </Button>
+        </Form.Item>
+        <Divider>Or</Divider>
+        <p className="text text-normal">
+          Chưa có tài khoản ?
+          <span>
+            <Link to="/register"> Đăng ký</Link>
+          </span>
+        </p>
+      </Form>
+    </div>
+  );
+};
+
+export default LoginPage;
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,75 +1,120 @@
-import React from 'react';
-import {Button, Divider, Form, Input} from 'antd';
+import React, { useState } from "react";
+import {
+  Button,
+  Divider,
+  Form,
+  Input,
+  Typography,
+  message,
+  notification
+} from "antd";
+import "./register.scss";
+import { useNavigate, Link } from "react-router-dom";
+import { callRegister } from "../../services/api";
 
 const RegisterPage = () => {
-    const onFinish = (values) =>{
-        console.log('Success: ', values)
+  const navigate = useNavigate();
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const onFinish = async (values) => {
+    const { fullName, email, password, phone } = values;
+    setIsSubmit(true);
+    const res = await callRegister(fullName, email, password, phone);
+    setIsSubmit(false);
+    if (res?.data?._id) {
+      message.success("Đăng ký tài khoản thành công!");
+      navigate("/login");
+    } else {
+      notification.error({
+        message: "Có lỗi xảy ra",
+        description:
+          res.message && Array.isArray(res.message)
+            ? res.message[0]
+            : res.message,
+        duration: 5
+      });
     }
+  };
 
-    return (
-    <div className='register-page' style={{padding: '50px'}}>
-        <h3 style={{textAlign: 'center'}}>Đăng ký người dùng mới</h3>
-        <Divider/>
-        <Form
-            name="basic"
-            labelCol={{ span: 6}}
-            style={{maxWidth: 600, margin: '0 auto'}}
-            onFinish={onFinish}
-            autoComplete='off'
+  return (
+    <div className="register-page">
+      <Form
+        className="loginForm"
+        name="basic"
+        labelCol={{ span: 6 }}
+        style={{ maxWidth: 600, margin: "0 auto" }}
+        onFinish={onFinish}
+        autoComplete="off"
+      >
+        <Typography.Title>Tạo mới tài khoản</Typography.Title>
+        <Divider></Divider>
+        <Form.Item
+          label="Họ và tên:"
+          name="fullName"
+          rules={[
+            {
+              required: true,
+              message: "fullname không được để trống"
+            }
+          ]}
         >
-            <Form.Item
-                labelCol={{ span: 24}}
-                label="Full name"
-                name="fullName"
-                rules={[{
-                    required: true,
-                    message: 'fullname không được để trống'
-                }]}
-            >
-                <Input/>
-            </Form.Item>
-    
-            <Form.Item
-                label="Email"
-                name="email"
-                rules={[{
-                    required: true,
-                    message: 'email không được để trống'
-                }]}
-            >
-                <Input/>
-            </Form.Item>
+          <Input />
+        </Form.Item>
 
-            <Form.Item
-                label="Password"
-                name="password"
-                rules={[{
-                    required: true,
-                    message: 'Please input your password'
-                }]}
-            >
-                <Input.Password/>
-            </Form.Item>
-    
-            <Form.Item
-                label="Phone"
-                name="phone"
-                rules={[{
-                    required: true,
-                    message: 'phone không được để trống'
-                }]}
-            >
-                <Input/>
-            </Form.Item>
+        <Form.Item
+          label="Email:"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "email không được để trống"
+            }
+          ]}
+        >
+          <Input />
+        </Form.Item>
 
-            <Form.Item wrapperCol={{ offset: 6, span: 16}}>
-                <Button type="primary" htmlType="submit" loading={false}>
-                    Register
-                </Button>
-            </Form.Item>
-        </Form>
+        <Form.Item
+          label="Password:"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password"
+            }
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="Phone:"
+          name="phone"
+          rules={[
+            {
+              required: true,
+              message: "phone không được để trống"
+            }
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item style={{ textAlign: "center" }}>
+          <Button type="primary" htmlType="submit" loading={isSubmit}>
+            Đăng ký
+          </Button>
+        </Form.Item>
+        <Divider>Or</Divider>
+        <p className="text text-normal">
+          Đã có tài khoản ?
+          <span>
+            <Link to="/login"> Đăng nhập</Link>
+          </span>
+        </p>
+      </Form>
     </div>
-    )
-}
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
